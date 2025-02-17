653eb839cb6d37fe203b0987ee7be760d3f31e4b
elisavonderforst <Elisa.vonderforst@studentfr.ch>
2025-02-17 22:43:01 +0100

    burger menu js hidden scroll

--- src/js/main.js
+++ src/js/main.js
@@ -9,13 +9,27 @@ var toggleMenu = function toggleMenu() {
 
 burger.addEventListener("click", toggleMenu);
 
-var toggleMenuClose = function toggleMenuClose() {
-  toggleMenuClose.classList("is-close");
-};
-
-aAbout.addEventListener("click", toggleMenu);
-
 // function agrandirImage() {
 //   const image = document.getElementById("img1");
 //   image.classList.toggle("agrandie"); // Ajoute ou retire la classe 'agrandie'
 // }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const burgerMenuButton = document.querySelector(".header-hidden");
+  let lastScrollTop = 0;
+
+  window.addEventListener("scroll", function () {
+    const currentScrollTop =
+      window.pageYOffset || document.documentElement.scrollTop;
+
+    console.log(currentScrollTop); // Check if scroll position is detected
+
+    if (currentScrollTop > lastScrollTop) {
+      burgerMenuButton.classList.add("hidden-scroll");
+    } else {
+      burgerMenuButton.classList.remove("hidden-scroll");
+    }
+
+    lastScrollTop = currentScrollTop;
+  });
+});
